7202a23e5f8cebf04e6f348854a64b254aa3f247
Francois Perrad <francois.perrad@gadz.org>
2010-11-25 16:49:13 +0100

    equiv XML::Simple

--- src/Spore/Middleware/Format/XML.lua
+++ src/Spore/Middleware/Format/XML.lua
@@ -1,10 +1,10 @@
 --
 -- lua-Spore : <http://fperrad.github.com/lua-Spore/>
 --
+-- see http://search.cpan.org/~grantm/XML-Simple/
 
-local error = error
-local pcall = pcall
-local require = require
+local pairs = pairs
+local tostring = tostring
 local type = type
 local raises = require 'Spore'.raises
 local xml = require 'lxp.lom'
@@ -12,10 +12,114 @@ local xml = require 'lxp.lom'
 _ENV = nil
 local m = {}
 
+local function escape (val, attr)
+    local str = tostring(val)
+    str = str:gsub('&', '&amp;')
+    str = str:gsub('<', '&lt;')
+    str = str:gsub('>', '&gt;')
+    if attr then
+        str = str:gsub('"', '&quot;')
+        str = str:gsub("'", '&apos;')
+    end
+    return str
+end
+
+local function element (name, t, options, level)
+    local indent = options.indent
+    local r = ''
+    if indent then
+        r = r .. indent:rep(level)
+    end
+    r = r .. '<' .. name
+    if type(t) == 'table' then
+        for k, v in pairs(t) do
+            if type(v) ~= 'table' then
+                r = r .. ' ' .. k .. '="' .. escape(v, true) .. '"'
+            end
+        end
+    end
+    r = r .. '>'
+    if indent and type(t) == 'table' then
+        r = r .. '\n'
+    end
+    if type(t) == 'table' then
+        for k, v in pairs(t) do
+            if type(v) == 'table' then
+                if #v > 0 then
+                    for i = 1, #v do
+                        r = r .. element(k, v[i], options, level+1)
+                    end
+                else
+                    local key_attr = options.key_attr or {}
+                    local key = key_attr[k]
+                    for kk, vv in pairs(v) do
+                        if key then
+                            vv[key] = kk
+                        end
+                        r = r .. element(k, vv, options, level+1)
+                    end
+                end
+            end
+        end
+    else
+        r = r .. escape(t)
+    end
+    if indent and type(t) == 'table' then
+        r = r .. indent:rep(level)
+    end
+    r = r .. '</' .. name .. '>'
+    if indent then
+        r = r .. '\n'
+    end
+    return r
+end
+
+local function to_xml (t, options)
+    for k, v in pairs(t) do
+        return element(k, v, options, 0)
+    end
+end
+
+local function collapse (doc, options)
+    local string_leaf = true
+    local t = {}
+    for k, v in pairs(doc.attr) do
+        if type(k) == 'string' then
+            t[k] = v
+            string_leaf = false
+        end
+    end
+    for i = 1, #doc do
+        local v = doc[i]
+        if type(v) == 'string' then
+            if not v:match '^%s+' then
+                t[#t+1] = v
+            end
+        else
+            local name = v.tag
+            t[name] = t[name] or {}
+            local tt = t[name]
+            local key_attr = options.key_attr or {}
+            local key = v.attr[key_attr[name]]
+            if key then
+                tt[key] = collapse(v, options)
+            else
+                tt[#tt+1] = collapse(v, options)
+            end
+            string_leaf = false
+        end
+    end
+    if string_leaf and #t <= 1 then
+        return t[1]
+    else
+        return t
+    end
+end
+
 function m:call (req)
     local spore = req.env.spore
     if spore.payload and type(spore.payload) == 'table' then
-        spore.payload = error "TODO"
+        spore.payload = to_xml(spore.payload, self)
         req.headers['content-type'] = 'text/xml'
     end
     req.headers['accept'] = 'text/xml'
@@ -23,7 +127,7 @@ function m:call (req)
                 if res.body then
                     local r, msg = xml.parse(res.body)
                     if r then
-                        res.body = r
+                        res.body = { [r.tag] = collapse(r, self) }
                     else
                         if spore.errors then
                             spore.errors:write(msg, "\n")
--- test/33-format_xml.t
+++ test/33-format_xml.t
@@ -6,7 +6,7 @@ if not pcall(require, 'lxp.lom') then
     skip_all 'no xml'
 end
 
-plan(12)
+plan(14)
 
 if not require_ok 'Spore.Middleware.Format.XML' then
     skip_rest "no Spore.Middleware.Format.XML"
@@ -14,12 +14,59 @@ if not require_ok 'Spore.Middleware.Format.XML' then
 end
 local mw = require 'Spore.Middleware.Format.XML'
 
+local payload = {
+    config = {
+        logdir = '/var/log/foo/',
+        debugfile = '/tmp/foo.debug',
+        server = {
+            sahara = {
+                address = {
+                    '10.0.0.101',
+                    '10.0.1.101',
+                },
+            },
+            gobi = {
+                address = {
+                    '10.0.0.102',
+                },
+            },
+            kalahari = {
+                address = {
+                    '10.0.0.103',
+                    '10.0.1.103',
+                },
+            },
+        },
+    },
+}
 local env = {
-    spore = {},
+    spore = {
+        payload = payload
+    },
 }
 local req = require 'Spore.Request'.new(env)
 local cb = mw.call({}, req)
 type_ok( cb, 'function', "returns a function" )
+is( env.spore.payload, [[<config logdir="/var/log/foo/" debugfile="/tmp/foo.debug"><server><address>10.0.0.101</address><address>10.0.1.101</address></server><server><address>10.0.0.102</address></server><server><address>10.0.0.103</address><address>10.0.1.103</address></server></config>]], "payload encoded")
+
+env.spore.payload = payload
+local req = require 'Spore.Request'.new(env)
+local cb = mw.call({ indent = '  ', key_attr = { server = 'name' } }, req)
+is( env.spore.payload, [[
+<config logdir="/var/log/foo/" debugfile="/tmp/foo.debug">
+  <server name="sahara">
+    <address>10.0.0.101</address>
+    <address>10.0.1.101</address>
+  </server>
+  <server name="gobi">
+    <address>10.0.0.102</address>
+  </server>
+  <server name="kalahari">
+    <address>10.0.0.103</address>
+    <address>10.0.1.103</address>
+  </server>
+</config>
+]], "payload encoded with options")
 
 local resp = {
     status = 200,
@@ -35,8 +82,8 @@ is( ret, resp, "returns same table" )
 is( ret.status, 200, "200 OK" )
 local data = ret.body
 type_ok( data, 'table' )
-is( data.attr.username, 'john', "username is john" )
-is( data.attr.password, 's3kr3t', "password is s3kr3t" )
+is( data.user.username, 'john', "username is john" )
+is( data.user.password, 's3kr3t', "password is s3kr3t" )
 
 resp.body = [[
 { INVALID }

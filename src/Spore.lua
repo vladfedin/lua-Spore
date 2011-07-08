81e44240cae04c8263ffbab35596bc7bb9188a4c
Francois Perrad <francois.perrad@gadz.org>
2011-07-08 12:34:09 +0200

    prepare release 0.1.4

--- src/Spore.lua
+++ src/Spore.lua
@@ -23,7 +23,7 @@ local m = {}
 
 m.early_validate = true
 
-local version = '0.1.3'
+local version = '0.1.4'
 
 local function raises (response, reason)
     local ex = { response = response, reason = reason }

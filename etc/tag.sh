1c76067e9c2d9df12a3049be0b34ac976d1de516
Alexey Romanoff <ar@logiceditor.com>
2021-06-27 08:42:47 +0300

    etc/tag.sh: fix tag name generation

--- etc/tag.sh
+++ etc/tag.sh
@@ -3,6 +3,6 @@
 set -euox pipefail
 
 BASE_TAG="0.3.3"
-TAG="$(git describe --tags --exclude "${BASE_TAG}-*-*" | sed 's/-/./g')"
+TAG="$(git describe --tags --exclude "${BASE_TAG}.*.*" | sed 's/-/./g')"
 git tag "${TAG}"
 git push origin "${TAG}"
